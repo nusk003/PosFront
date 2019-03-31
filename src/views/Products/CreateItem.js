0ecf54690b79d36b98e0a498ad11cf0dd4951fc3
nusky <nusk003@gmail.com>
2019-03-31 11:37:34 +0530

    First upload

---
+++ src/views/Products/CreateItem.js
@@ -0,0 +1,12 @@
+import React from 'react'
+
+class CreateItem extends React.Component{
+
+    render(){
+        return(
+            <h1>Insert Item</h1>
+        )
+    }
+}
+
+export default CreateItem
---
+++ src/views/Products/EditItem.js
@@ -0,0 +1,12 @@
+import React from 'react'
+
+class EditItem extends React.Component{
+
+    render(){
+        return(
+            <h1>Edit Item</h1>
+        )
+    }
+}
+
+export default EditItem
